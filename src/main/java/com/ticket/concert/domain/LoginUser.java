c9f41592f225ede39715e2f93c2b86f77303b6fc
CHANHAN <130114269+chanani@users.noreply.github.com>
2026-05-08 10:14:38 +0900

    refactor : serializable 제거 및 role 배열로 변경

--- src/main/java/com/ticket/concert/domain/LoginUser.java
+++ src/main/java/com/ticket/concert/domain/LoginUser.java
@@ -1,11 +1,8 @@
 package com.ticket.concert.domain;
 
-import java.io.Serializable;
-
-// 세션에 담기는 객체를 Serializable을 활용해 직렬화
 public record LoginUser(
         Long id,
         String username,
-        String role
-) implements Serializable {
+        String[] role
+) {
 }
