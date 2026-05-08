827f3f07978ca8db491746cbb42cb9cf479e992d
CHANHAN <130114269+chanani@users.noreply.github.com>
2026-05-08 21:39:52 +0900

    refactor : regex에 EMAIL 정규식 추가

--- src/main/java/com/ticket/concert/global/regex/UserRegex.java
+++ src/main/java/com/ticket/concert/global/regex/UserRegex.java
@@ -2,7 +2,7 @@ package com.ticket.concert.global.regex;
 
 public class UserRegex {
 
-    public static final String USER_ID = "^[A-Za-z0-9]{8,20}$";
     // 8~20자, 숫자/영문/특수문자 중 2종류 이상 포함
     public static final String PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d)|(?=.*[A-Za-z])(?=.*[!@#$%^&*])|(?=.*\\d)(?=.*[!@#$%^&*]).{8,20}$";
+    public static final String EMAIL = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
 }
