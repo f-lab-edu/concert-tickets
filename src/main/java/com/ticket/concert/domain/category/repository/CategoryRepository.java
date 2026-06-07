13e2adf0ce7cfacfff653e8d6301cbe3e29c0e1b
CHANHAN <130114269+chanani@users.noreply.github.com>
2026-06-07 18:05:27 +0900

    test : category 테스트 코드 작성

--- src/main/java/com/ticket/concert/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/ticket/concert/domain/category/repository/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.ticket.concert.domain.category.repository;
 import com.ticket.concert.domain.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+import java.util.Optional;
 
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByName(String name);
+    Optional<Category> findByName(String name);
 }
