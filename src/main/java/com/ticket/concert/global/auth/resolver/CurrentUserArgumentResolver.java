c0363f42f4a58bba178d424ef70ce194d14c261f
CHANHAN <130114269+chanani@users.noreply.github.com>
2026-06-24 20:25:15 +0900

    feat : User 리졸버 생성

---
+++ src/main/java/com/ticket/concert/global/auth/resolver/CurrentUserArgumentResolver.java
@@ -0,0 +1,26 @@
+package com.ticket.concert.global.auth.resolver;
+
+import com.ticket.concert.domain.LoginUser;
+import com.ticket.concert.global.auth.UserContextHolder;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUser.class)
+                && LoginUser.class.isAssignableFrom(parameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+        return UserContextHolder.get();
+    }
+}
